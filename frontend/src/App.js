54d461e638f0bdb015b7265bfe1fba582872ee6e
LeeYena <leeyena@192.168.nate.com>
2024-07-23 23:34:03 +0900

    대출 api, 반납 api 연결'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishedDate, setPublishedDate] = useState('');
+  const [id, setID] = useState(0);
+
+  //대출
+  const handleDeleteBook = async () => {
+    const response = await fetch(`http://localhost:5000/api/books/${id}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      const book = await response.json();
+      alert(`${book.title}을 대출하셨습니다.`);
+    } else {
+      alert('대출이 되지 않았습니다.');
+    }
+  };
+
+  //반납
+  const handleAddBook = async () => {
+    const response = await fetch('http://localhost:5000/api/books/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title, author, publishedDate }),
+    });
+
+    if (response.ok) {
+      const book = await response.json();
+      alert(`${book.title}을 반납하셨습니다.`);
+    } else {
+      alert('반납이 되지 않았습니다.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Library Management</h1>
+      <h2>Add Book</h2>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Book Title" required />
+      <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author" required />
+      <input type="date" value={publishedDate} onChange={(e) => setPublishedDate(e.target.value)} />
+      <input value={id} onChange={(e) => setID(e.target.value)} />
+      <button onClick={handleDeleteBook}>대출하기</button>
+      <button onClick={handleAddBook}>반납하기</button>
     </div>
   );
 }
